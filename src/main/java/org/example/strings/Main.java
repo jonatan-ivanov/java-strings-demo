6bcfed6f39926b2da9a58f6fa5a3f33b572c98bf
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-12-01 20:53:27 -0800

    Update Main.java

--- src/main/java/org/example/strings/Main.java
+++ src/main/java/org/example/strings/Main.java
@@ -1,6 +1,5 @@
 package org.example.strings;
 
-import static java.lang.String.format;
 import static java.lang.System.getProperty;
 
 import java.util.Collection;
@@ -13,28 +12,38 @@ public class Main {
         "Java",
         "我喜欢茶",
         "𝕒𝕓𝕔",
-        "🧑❤☕",
-        "🧑‍💻❤️🍵"
+        "👩❤☕",
+        "👩‍💻❤️🍵"
     );
 
     public static void main(String[] args) {
         System.out.printf("%s %s (%s)%n", getProperty("java.vm.vendor"), getProperty("java.vm.version"), getProperty("java.vm.name"));
 
-        System.out.println("\n--- length ---");
+        System.out.println("\n--- Length ---");
         transformAndPrint(TEST_STRINGS, String::length);
 
-        System.out.println("\n--- codeUnits ---");
-        transformAndPrint(TEST_STRINGS, Main::codeUnits);
+        System.out.println("\n--- Code Point Count ---");
+        transformAndPrint(TEST_STRINGS, Main::codePointCount);
 
-        System.out.println("\n--- codePoints ---");
+        System.out.println("\n--- Code Points ---");
         transformAndPrint(TEST_STRINGS, Main::codePoints);
 
-        System.out.println("\n--- reverse ---");
+        System.out.println("\n--- Code Units ---");
+        transformAndPrint(TEST_STRINGS, Main::codeUnits);
+
+        System.out.println("\n--- Buggy Reverse ---");
+        transformAndPrint(TEST_STRINGS, Main::buggyReverse);
+
+        System.out.println("\n--- Reverse ---");
         transformAndPrint(TEST_STRINGS, Main::reverse);
     }
 
     private static void transformAndPrint(Collection<String> collection, Function<String, ?> function) {
         collection.forEach(item -> System.out.println(item + " -> " + function.apply(item)));
+    }
+
+    private static int codePointCount(String str) {
+        return str.codePointCount(0, str.length());
     }
 
     private static String codeUnits(String str) {
@@ -52,6 +61,16 @@ public class Main {
             .map(hex -> "U+" + hex)
             .collect(Collectors.joining(" "));
     }
+
+    private static String buggyReverse(String original) {
+        String result = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            result += original.charAt(i);
+        }
+
+        return result;
+    }
+
 
     private static String reverse(String str) {
         return new StringBuilder(str).reverse().toString();
